d993f6266bae358eff9e3eb9c735b43e623d85c7
uf-code <ugur.firat.developer@gmail.com>
2021-11-30 16:16:04 +0300

    operators

---
+++ 02_programming-fundamentals/08-exercise.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// ==
+	x := (10 == 10)
+	fmt.Println(x)
+
+	// !=
+	y := (10 != 20)
+	fmt.Println(y)
+
+	// <=
+	z := (10 <= 20)
+	fmt.Println(z)
+
+	// >=
+	a := (10 >= 20)
+	fmt.Println(a)
+
+	// <
+	b := (10 < 20)
+	fmt.Println(b)
+
+	// >
+	c := (10 > 20)
+	fmt.Println(c)
+
+}
