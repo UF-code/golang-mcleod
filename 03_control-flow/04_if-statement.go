47c4251fe41a9562dfd182227a213e8e648af507
uf-code <ugur.firat.developer@gmail.com>
2021-12-01 23:35:17 +0300

    if statement

---
+++ 03_control-flow/04_if-statement.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	if true {
+		fmt.Println(`01`)
+	}
+	if false {
+		fmt.Println(`Not Gonna Show This One`)
+	}
+	if !false {
+		fmt.Println(`03`)
+	}
+	if !true {
+		fmt.Println(`Not Gonna Show This One`)
+	}
+
+	if x := 42; x == 41 {
+		fmt.Println(`01`)
+	} else {
+		fmt.Println(x)
+	}
+
+	fmt.Println("Here's a Statement")
+
+}
