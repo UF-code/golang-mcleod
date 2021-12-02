3403be50a26cb5a196e366b612cbe759f0fca58d
uf-code <ugur.firat.developer@gmail.com>
2021-12-02 21:21:50 +0300

    switch case

--- 03_control-flow/06_switch.go
+++ 03_control-flow/06_switch.go
@@ -11,7 +11,22 @@ func main() {
 		fmt.Println("This is not gonna print")
 	case (2 == 2):
 		fmt.Println("This is gonna print")
+		// even if is not true if you use fallthrough it will print
+		fallthrough
 	case true:
-		fmt.Println("This is not gonna print")
+		fmt.Println("This is also gonna print")
+	default:
+		fmt.Println("Default")
+	}
+
+	switch "Bond" {
+	case "Money":
+		fmt.Println("Not gonna show")
+	case "Power":
+		fmt.Println("Will not appear")
+	case "Bond":
+		fmt.Println("This will appear")
+	default:
+		fmt.Println("Test")
 	}
 }
