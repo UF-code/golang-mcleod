d5b684308755da33f8b84af9f065b59c6c857808
uf-code <ugur.firat.developer@gmail.com>
2021-12-06 19:55:17 +0300

    slice for

--- 04_grouping-data/02_slice.go
+++ 04_grouping-data/02_slice.go
@@ -5,6 +5,12 @@ import "fmt"
 func main() {
 	// x := type{values} // composit literal
 	x := []int{4, 5, 7, 8, 42}
+
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
 	fmt.Println(x)
 
+	for i, v := range x {
+		fmt.Println(i, v)
+	}
 }
