d5b3e7e68aa02f634ccf6b657f4b2ad3f95ed87b
uf-code <ugur.firat.developer@gmail.com>
2021-12-06 21:17:56 +0300

    map delete

--- 04_grouping-data/05_map.go
+++ 04_grouping-data/05_map.go
@@ -11,14 +11,23 @@ func main() {
 	fmt.Println(m["James"])
 
 	//
-	//
+	// map if there is item
 	if v, ok := m["Barnabas"]; ok {
 		fmt.Println(v)
 	}
 
+	//
+	// map add
 	m["Todd"] = 43
 
 	for k, v := range m {
 		println(k, v)
 	}
+
+	//
+	// map delete
+
+	delete(m, "James")
+
+	fmt.Println(m)
 }
