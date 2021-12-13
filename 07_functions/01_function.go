ca1bab47cf1d7e9577077ec143060dc470924dd4
uf-code <ugur.firat.developer@gmail.com>
2021-12-13 05:10:45 +0300

    functions parameters returns

---
+++ 07_functions/01_function.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	foo()
+	bar("James")
+	s1 := woo("Moneypenny")
+	fmt.Println(s1)
+
+	x, y := mouse("Ian", "Flemning")
+	fmt.Println(x, y)
+
+}
+
+func foo() {
+	fmt.Println("Foo")
+}
+
+func bar(s string) {
+	fmt.Println("Hello ", s)
+}
+
+func woo(s string) string {
+	return fmt.Sprint("Hello from woo,", s)
+}
+
+func mouse(fn string, ln string) (string, bool) {
+	a := fmt.Sprint(fn, ln, `, says "Hello"`)
+	b := false
+	return a, b
+}
