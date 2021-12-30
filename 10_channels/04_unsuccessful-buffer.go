e146e638fced1751c78dfe16023e8dfbe0ee93b4
uf-code <ugur.firat.developer@gmail.com>
2021-12-30 14:37:45 +0300

    buffer

--- 10_channels/04_unsuccessful-buffer.go
+++ 10_channels/04_unsuccessful-buffer.go
@@ -4,10 +4,13 @@ import "fmt"
 
 func main() {
 	c := make(chan int, 2)
+	fmt.Printf("%T\n", c)
 
 	c <- 42
 	c <- 43
 
 	fmt.Println(<-c)
 	fmt.Println(<-c)
+	fmt.Println("-----")
+	fmt.Printf("%T\n", c)
 }
