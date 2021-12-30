ea08b6b4dbae763c57016419b59abd647e3edcb6
uf-code <ugur.firat.developer@gmail.com>
2021-12-31 00:43:25 +0300

    exercise 10

---
+++ 11_error_handling/10_exercise.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type person struct {
+	First   string
+	Last    string
+	Sayings []string
+}
+
+func main() {
+	p1 := person{
+		First:   "James",
+		Last:    "Bond",
+		Sayings: []string{"Shaken, not stirred", "Any last wishes?", "Never say never"},
+	}
+
+	bs, err := json.Marshal(p1)
+	if err != nil {
+		log.Fatalln("JSON did not marshal - here's the error:", err)
+	}
+	fmt.Println(string(bs))
+}
+
+// toJSON needs to return an error also
+func toJSON(a interface{}) ([]byte, error) {
+	bs, err := json.Marshal(a)
+	if err != nil {
+		return []byte{}, fmt.Errorf("There was an error in to JSON: %v", err)
+	}
+	return bs, nil
+
+}
